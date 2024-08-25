f3ce88cbe8cedfe5f97246f102ff8d51a4df899d
Rodrigo Hamada <104478788+rodrigohamada@users.noreply.github.com>
2024-08-25 09:40:19 -0300

    Update MobileMenuIcon.js

--- projetoweb/src/components/MobileMenuIcon.js
+++ projetoweb/src/components/MobileMenuIcon.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "../styles/MobileMenuIcon.css";
 
-const MobileMenuIcon = () => {
+const MobileMenuIcon = ({ onClick, isOpen }) => {
   return (
-    <div className="mobile-menu-icon">
-      <span></span>
-      <span></span>
-      <span></span>
+    <div className={`mobile-menu-icon ${isOpen ? "open" : ""}`} onClick={onClick}>
+      <span className="bar"></span>
+      <span className="bar"></span>
+      <span className="bar"></span>
     </div>
   );
 };
