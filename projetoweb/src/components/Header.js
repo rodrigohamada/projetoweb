492329943066a157e778124d997600cd8ca177eb
Rodrigo Hamada <104478788+rodrigohamada@users.noreply.github.com>
2024-08-25 09:39:56 -0300

    Update Header.js

--- projetoweb/src/components/Header.js
+++ projetoweb/src/components/Header.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom"; 
 import { auth } from "../store/env";
-import MobileMenuIcon from "./MobileMenuIcon";
 import logo from "../assets/images/logo.png";
+import menuIcon from "../assets/images/MobileMenuIcon.png";
 
 const Header = () => {
-  const [user, setUser] = useState(null); // Estado para armazenar o usuário logado
+  const [user, setUser] = useState(null);
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false); // Estado para controlar o menu móvel
 
   useEffect(() => {
     document.title = "Organic - Cestas Orgânicas";
@@ -14,34 +15,37 @@ const Header = () => {
       link.href = require("../assets/images/folha.png");
     }
 
-    // Verifica o estado de autenticação do usuário
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
-        setUser(user); // Usuário está logado
+        setUser(user);
       } else {
-        setUser(null); // Usuário não está logado
+        setUser(null);
       }
     });
 
-    return () => unsubscribe(); // Limpa o listener de autenticação quando o componente é desmontado
+    return () => unsubscribe();
   }, []);
 
   const handleLogout = () => {
     auth.signOut();
-    setUser(null); // Limpa o estado de usuário após logout
-    window.location.href = '/'; // Redireciona para a página inicial após o logout
+    setUser(null);
+    window.location.href = '/';
+  };
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen); // Alterna o estado do menu móvel
   };
 
   return (
     <header>
       <Link to="/"><img src={logo} alt="Logo Cooperativa Orgânica" className="logo" /></Link>
       <nav>
-        <ul className="menu">
+        <ul className={`menu ${isMobileMenuOpen ? "open" : ""}`}> {/* Aplica a classe "open" se o menu móvel estiver aberto */}
           <li><Link to="/" id="home-link">Home</Link></li>
           <li><Link to="/sobre">Sobre</Link></li>
           <li><Link to="/produtos">Produtos</Link></li>
           <li><Link to="/cadastro_pedidos" id="pedidos-link">Pedidos</Link></li>
-          {user ? ( // Se o usuário estiver logado, esconde o link de login
+          {user ? (
             <>
               <li><Link to="/cadastro_produtos" id="cadastro-produtos-link">Cadastro de Produtos</Link></li>
               <button id="logout-button" onClick={handleLogout}>Logout</button>
@@ -51,7 +55,17 @@ const Header = () => {
             <li><Link to="/login" id="login-link">Login</Link></li>
           )}
         </ul>
-        <MobileMenuIcon />
+        <div className="mobile-menu-controls">
+          {isMobileMenuOpen && (
+            <button className="close-menu" onClick={toggleMobileMenu}>x</button>
+          )}
+          <img
+            src={menuIcon}
+            alt="Menu"
+            className="mobile-menu-icon"
+            onClick={toggleMobileMenu}
+          />
+        </div>
       </nav>
     </header>
   );
